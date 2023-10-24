c9fd1ccbfc707e1652f927ae75debb87e7a43c94
Nate Smith <Smithers1211@gmail.com>
2023-10-24 16:52:41 -0400

    Making a String calculator

---
+++ stringcalculator-tuesday/StringCalculator.cs
@@ -0,0 +1,54 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers, char delimiter)
+    {
+        int result = 0;
+        int absValResult = 0;
+        List<int> negatives = new List<int>();
+        if (numbers == "")
+        {
+            return result;
+
+        }
+        else
+        {
+            //Replace the newline with delimiter
+            numbers = numbers.Replace('\n', delimiter);
+
+            //Split the string of numbers
+            string[] strings = numbers.Split(delimiter);
+
+            //Loop through the array
+            for (int i = 0; i < strings.Length; i++)
+            {
+                //Find the absolute value result
+                absValResult += Math.Abs(int.Parse(strings[i]));
+                //Find the normal result
+                result += int.Parse(strings[i]);
+                //Find the negatives and add them to the list
+                if (int.Parse(strings[i]) < 0)
+                {
+                    negatives.Add(int.Parse(strings[i]));
+                }
+            }
+
+        }
+
+        if (absValResult != result)
+        {
+            //Throw negative number exception. List the numbers
+            throw new Exception($"Negatives not allowed: {negatives.ToString()}");
+        }
+        else
+        {
+            return result;
+        }
+
+
+    }
+
+}
---
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -0,0 +1,61 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("", ',');
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("5", 5)]
+    [InlineData("20", 20)]
+    public void SingleDigits(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input, ',');
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,1", 2)]
+    [InlineData("3,5,6", 14)]
+    [InlineData("3,15,36", 54)]
+    public void TwoDigits(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input, ',');
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2,3", 6)]
+    [InlineData("1\n3,15\n34,6", 59)]
+    public void MultiDigitsWithNewLine(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input, ',');
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2%3", 6, '%')]
+    [InlineData("1?3?6", 10, '?')]
+    [InlineData("1\n3;15\n34;6", 59, ';')]
+    [InlineData("//;\n1;2", 3, ';')]
+    public void MultiDigitsWithNewLineAndDelimiters(string input, int expected, char delimiter)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input, delimiter);
+        Assert.Equal(expected, result);
+    }
+
+
+}
