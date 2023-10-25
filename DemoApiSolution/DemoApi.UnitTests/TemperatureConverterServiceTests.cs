7d4a2016a1559b5d2de7d0de522a1e6635b741f8
Nate Smith <Smithers1211@gmail.com>
2023-10-25 16:52:08 -0400

    Temperature Converter Day 3

---
+++ DemoApiSolution/DemoApi.UnitTests/TemperatureConverterServiceTests.cs
@@ -0,0 +1,43 @@
+﻿
+using DemoApi.Services;
+
+namespace DemoApi.UnitTests;
+public class TemperatureConverterServiceTests
+{
+
+    [Fact]
+    public void UsesTheFeeCalculator()
+    {
+        //Given
+
+        var fakeFeeCalculator = Substitute.For<ICalculateFees>();
+        fakeFeeCalculator.GetCurrentFee().Returns(42.23M);
+        var service = new TemperatureConverterService(fakeFeeCalculator);
+        //When
+        ConversionWithFeeResponse response = service.ConvertFtoC(100F);
+        //Then
+        Assert.Equal(42.23M, response.Fee);
+    }
+}
+//Test Double
+public class StubbedFeeThing : ICalculateFees
+{
+    public decimal GetCurrentFee()
+    {
+        return 5;
+    }
+}
+
+
+/* {
+	"f":100,
+	"c":37.77778,
+	"fee": 0	
+}
+ 
+ 
+{
+	"f":100,
+	"c":37.77778
+	"fee": 0.03	
+} */
