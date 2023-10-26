8e386b5748c9c89da6720619f31414292291608b
Nate Smith <Smithers1211@gmail.com>
2023-10-26 16:48:12 -0400

    Day 4 additions

---
+++ DemoApiSolution/DemoApi.ContractTests/Todos/GettingTodos.cs
@@ -0,0 +1,25 @@
+﻿using Alba;
+
+namespace DemoApi.ContractTests.Todos;
+public class GettingTodos
+{
+
+    [Fact]
+    public async Task CanGetATodo()
+    {
+        var host = await AlbaHost.For<Program>();
+
+        await host.Scenario(api =>
+        {
+            api.Get.Url("/todo-list/5481ece9-3363-472d-9b44-8a850ee2f24d");
+            api.StatusCodeShouldBeOk();
+        });
+
+        await host.Scenario(api =>
+        {
+            api.Get.Url("/todo-list/" + Guid.NewGuid().ToString());
+            api.StatusCodeShouldBe(404);
+
+        });
+    }
+}
--- DemoApiSolution/DemoApi/Program.cs
+++ DemoApiSolution/DemoApi/Program.cs
@@ -1,4 +1,5 @@
 using DemoApi.Services;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,18 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<TemperatureConverterService>();
 builder.Services.AddScoped<ICalculateFees, StandardFeeCalculator>();
 builder.Services.AddScoped<ISystemTime, SystemTime>();
+builder.Services.AddScoped<TodoListService>();
+
+var connectionString = builder.Configuration.GetConnectionString("database") ?? throw new Exception("No Database");
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+    if (builder.Environment.IsDevelopment())
+    {
+        options.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.All;
+    }
+});
 
 var app = builder.Build();
 
@@ -18,6 +31,32 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("todo-list/{id:Guid}", async (Guid id, TodoListService service) =>
+{
+    TodoItemCreated? item = await service.GetTodoItemAsync(id);
+    if (item is null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        return Results.Ok(item);
+    }
+});
+
+
+app.MapGet("/todo-list", async (TodoListService service) =>
+{
+    var response = await service.GetAllAsync();
+    return Results.Ok(response);
+});
+
+app.MapPost("/todo-list", async (CreateToDoItem item, TodoListService service) =>
+{
+    var response = await service.CreateTodoItem(item);
+    return Results.Ok(response);
+});
 
 app.MapGet("/temperatures/farenheit/{temp:float}/celcius", (float temp, TemperatureConverterService service) =>
 {
